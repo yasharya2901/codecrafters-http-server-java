6db65ca709b2b111bdadc13e091d5c6def050615
yasharya2901 <yasharya2901@gmail.com>
2024-09-03 17:11:02 +0530

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,7 +16,8 @@ public class Main {
        // ensures that we don't run into 'Address already in use' errors
        serverSocket.setReuseAddress(true);
 
-       serverSocket.accept(); // Wait for connection from client.
+       Socket clientRequest = serverSocket.accept(); // Wait for connection from client.
+         clientRequest.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
        System.out.println("accepted new connection");
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
