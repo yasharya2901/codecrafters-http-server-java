09d69b2ac498e2f7115395a09c02a9f271c08dbd
yasharya2901 <yasharya2901@gmail.com>
2024-09-03 20:01:44 +0530

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,96 +7,103 @@ import java.net.Socket;
 
 public class Main {
   public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
-     try {
-       ServerSocket serverSocket = new ServerSocket(4221);
+    try {
+      ServerSocket serverSocket = new ServerSocket(4221);
+      serverSocket.setReuseAddress(true);
 
-       // Since the tester restarts your program quite often, setting SO_REUSEADDR
-       // ensures that we don't run into 'Address already in use' errors
-       serverSocket.setReuseAddress(true);
-        while(true) {
-           Socket clientRequest = serverSocket.accept(); // Wait for connection from client.
-           System.out.println("accepted new connection");
-             InputStream input = clientRequest.getInputStream();
+      while (true) {
+        Socket clientRequest = serverSocket.accept();
+        System.out.println("accepted new connection");
 
-
-             BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-             String httpRequest = reader.readLine();
-             // httpRequest is something like "GET / HTTP/1.1" or "GET /index.html HTTP/1.1"
-             // The first part is the request method.
-             // The second part is the path.
-             // The third part is the HTTP version.
-
-             System.out.println("Received request: " + httpRequest);
-
-             if (httpRequest == null) {
-                 continue;
-             }
-
-             String[] requestParts = httpRequest.split(" ");
-             String path = requestParts[1];
-             System.out.println("Path: "+path);
-             processRequest(path, clientRequest);
+        InputStream input = clientRequest.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+        String httpRequest = reader.readLine();
+        if (httpRequest == null) {
+          continue;
         }
 
-     } catch (IOException e) {
-       System.out.println("IOException: " + e.getMessage());
-     } catch (Exception e) {
-         System.out.println("Exception: " + e.getMessage());
-     }
+        StringBuilder httpRequestHeader = new StringBuilder();
+        String line;
+
+        // Read the headers
+        while (!(line = reader.readLine()).isEmpty()) {
+          httpRequestHeader.append(line).append("\n");
+        }
+
+        // Read the body
+        StringBuilder httpRequestBody = new StringBuilder();
+        while (reader.ready() && (line = reader.readLine()) != null) {
+          httpRequestBody.append(line).append("\n");
+        }
+
+        System.out.println("Received request:\n" + httpRequest + "\n");
+        System.out.println("Received request header:\n" + httpRequestHeader + "\n");
+        System.out.println("Received request body:\n" + httpRequestBody + "\n");
+
+        String[] requestParts = httpRequest.split(" ");
+        String path = requestParts[1];
+        System.out.println("Path: " + path);
+        processRequest(path, httpRequestHeader.toString(), httpRequestBody.toString(), clientRequest);
+      }
+
+    } catch (IOException e) {
+      System.out.println("IOException: " + e.getMessage());
+    } catch (Exception e) {
+      System.out.println("Exception: " + e.getMessage());
+    }
   }
 
   private static String getHttpResponse(int responseCode, String responseMessage, String message) {
-        return "HTTP/1.1 "+responseCode+" "+responseMessage+"\r\n" +
-                "Content-Type: text/plain\r\n" +
-                "Content-Length: "+ message.length()+"\r\n" +
-                "\r\n" +
-                message;
+    return "HTTP/1.1 " + responseCode + " " + responseMessage + "\r\n" +
+           "Content-Type: text/plain\r\n" +
+           "Content-Length: " + message.length() + "\r\n" +
+           "\r\n" +
+           message;
   }
 
-  private static void processRequest(String path, Socket clientRequest) throws IOException {
-      String endLine = "-----------------------------------------------------------------";
-      String[] pathParts = path.split("/");
-      for (int i = 0; i < pathParts.length; i++) {
-            System.out.println("Path part:["+i+"]: " + pathParts[i]);
-      }
-      if (pathParts.length == 0) {
-          // The path is "/"
-          String message = "Welcome to the Homepage!";
+  private static void processRequest(String path, String httpRequestHeader, String httpRequestBody, Socket clientRequest) throws IOException {
+    String endLine = "-----------------------------------------------------------------";
+    String[] pathParts = path.split("/");
+    for (int i = 0; i < pathParts.length; i++) {
+      System.out.println("Path part:[" + i + "]: " + pathParts[i]);
+    }
+    if (pathParts.length == 0) {
+      String message = "Welcome to the Homepage!";
+      String httpResponse = getHttpResponse(200, "OK", message);
+      System.out.println("Sending response: " + httpResponse);
+      System.out.println(endLine);
+      clientRequest.getOutputStream().write(httpResponse.getBytes());
+    } else if (pathParts.length != 0) {
+      if (pathParts.length >= 2 && pathParts[1].equals("echo")) {
+        if (pathParts.length == 2) {
+          String message = "Please provide a message to echo";
           String httpResponse = getHttpResponse(200, "OK", message);
           System.out.println("Sending response: " + httpResponse);
           System.out.println(endLine);
           clientRequest.getOutputStream().write(httpResponse.getBytes());
-      }
-
-      else if (pathParts[1].equals("echo")) {
-          // The path is "/echo"
-          if (pathParts.length == 2) {
-              String message = "Please provide a message to echo";
-              String httpResponse = getHttpResponse(200, "OK", message);
-              System.out.println("Sending response: " + httpResponse);
-              System.out.println(endLine);
-              clientRequest.getOutputStream().write(httpResponse.getBytes());
-          }
-
-          else if (pathParts.length == 3) {
-              String message = pathParts[2];
-              String httpResponse = getHttpResponse(200, "OK", message);
-              System.out.println("Sending response: " + httpResponse);
-              System.out.println(endLine);
-              clientRequest.getOutputStream().write(httpResponse.getBytes());
-          }
-      } else {
-          // The path is not recognized
-          String message = "The requested path is not available";
-            String httpResponse = getHttpResponse(404, "Not Found", message);
-            System.out.println("Sending response: " + httpResponse);
+        } else if (pathParts.length == 3) {
+          String message = pathParts[2];
+          String httpResponse = getHttpResponse(200, "OK", message);
+          System.out.println("Sending response: " + httpResponse);
           System.out.println(endLine);
-            clientRequest.getOutputStream().write(httpResponse.getBytes());
+          clientRequest.getOutputStream().write(httpResponse.getBytes());
+        }
+      } else if (pathParts[1].equals("user-agent")) {
+        String message = httpRequestHeader;
+        String httpResponse = getHttpResponse(200, "OK", message);
+        System.out.println("Sending response: " + httpResponse);
+        System.out.println(endLine);
+        clientRequest.getOutputStream().write(httpResponse.getBytes());
+      } else {
+        String message = "The requested path is not available";
+        String httpResponse = getHttpResponse(404, "Not Found", message);
+        System.out.println("Sending response: " + httpResponse);
+        System.out.println(endLine);
+        clientRequest.getOutputStream().write(httpResponse.getBytes());
       }
+    }
+    clientRequest.close();
   }
 }
